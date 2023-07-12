312fa3283e2951322e81787c8ea4f671e7444d3e
HopeMarwal <vremea.dr@gmail.com>
2023-07-12 14:36:44 +0300

    Api fetch id fix cart items

--- app/api/cart/[id]/route.js
+++ app/api/cart/[id]/route.js
@@ -2,11 +2,12 @@ import { connectToDb } from "@/utils/database"
 import Cart from "@/models/cart";
 
 // Get user cart with products data 
-export const GET = async ({ params }) => {
+export const GET = async (req, { params }) => {
+  console.log(params)
   try {
     await connectToDb();
     const cartItems = await Cart.findOne({user: params.id}).populate('products.product_id')
-    
+    console.log(cartItems)
     return new Response(JSON.stringify(cartItems), { status: 200 })
 
   } catch (error) {
